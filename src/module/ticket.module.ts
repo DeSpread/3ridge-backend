f0b1dcc4b32fffde96717f41fad68c9a4203d022
cryptogang.btc <cryptodevgang@gmail.com>
2022-12-21 16:59:16 +0900

    feature: add register quest schmea into ticket

--- src/module/ticket.module.ts
+++ src/module/ticket.module.ts
@@ -4,10 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { TicketResolver } from '../infra/graphql/resolver/ticket.resolver';
 import { TicketService } from '../infra/database/service/ticket.service';
 import { Ticket, TicketSchema } from '../infra/schema/ticket.schema';
+import { Quest, QuestSchema } from '../infra/schema/quest.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Ticket.name, schema: TicketSchema }]),
+    MongooseModule.forFeature([
+      { name: Ticket.name, schema: TicketSchema },
+      { name: Quest.name, schema: QuestSchema },
+    ]),
     ConfigModule,
   ],
   providers: [TicketResolver, TicketService],
