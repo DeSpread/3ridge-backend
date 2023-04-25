a5d239ab04c6af99acf99676dbc301ad6252bea5
cryptogang.btc <cryptodevgang@gmail.com>
2023-04-25 14:58:31 +0900

    feature: add sui chain type to user's wallet

--- src/constant/chain.type.ts
+++ src/constant/chain.type.ts
@@ -3,6 +3,7 @@ import { registerEnumType } from '@nestjs/graphql';
 export enum ChainType {
   EVM = 'EVM',
   APTOS = 'APTOS',
+  SUI = 'SUI',
 }
 
 registerEnumType(ChainType, { name: 'ChainType' });
