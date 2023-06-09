d1c372de2402d8c477c1f180f6bc8ffe82bf125b
cryptogang.btc <cryptodevgang@gmail.com>
2023-06-09 18:22:31 +0900

    fix: changed TZ env

--- src/util/date.util.ts
+++ src/util/date.util.ts
@@ -1,6 +1,6 @@
 import moment from 'moment-timezone';
 
 export function momentNewDate(): Date {
-  const momentDate = moment().tz(process.env.TZ).format();
+  const momentDate = moment().tz('Asia/Seoul').format();
   return new Date(momentDate);
 }
