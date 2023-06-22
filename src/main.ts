2d818ecd384a8417a10aacf274076e31a5d7e110
cryptogang.btc <cryptodevgang@gmail.com>
2023-06-22 18:32:37 +0900

    chore: add comment block for todo list

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
 
+  // TODO: exception layer에서 DI가 안되는 이슈가 있어서 테스트가 필요
   // const loggerService = app.get(LoggerService);
   // app.useGlobalFilters(new AllExceptionsFilter(loggerService));
   await app.listen(3000);
