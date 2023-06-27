ef030870f7a818c039c422f0eb0a500076984e41
Kim Min Su <kms860226@naver.com>
2023-06-27 15:29:04 +0900

    feature: config aptos indexer url per env

--- src/module/verifier.module.ts
+++ src/module/verifier.module.ts
@@ -1,21 +1,26 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import {ConfigModule, ConfigService} from '@nestjs/config';
 import { VerifierResolver } from '../infra/graphql/resolver/verifier.resolver';
 import { VerifierService } from '../service/verifier.service';
 import { UserModule } from './user.module';
 import { GraphQLRequestModule } from '@golevelup/nestjs-graphql-request';
 
+
 @Module({
   imports: [
     ConfigModule,
     UserModule,
-    GraphQLRequestModule.forRoot(GraphQLRequestModule, {
-      endpoint: 'https://indexer.mainnet.aptoslabs.com/v1/graphql',
-      options: {
-        headers: {
-          'content-type': 'application/json',
+    GraphQLRequestModule.forRootAsync(GraphQLRequestModule, {
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        endpoint: configService.get<string>('aptos.indexerEndpoint'),
+        options: {
+          headers: {
+            'content-type': 'application/json',
+          },
         },
-      },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [VerifierResolver, VerifierService],
